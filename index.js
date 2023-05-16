be60805c4d3098ff7ed921916d19643793ed9af7
Zeeshan Haider <zeeshanhaider1902@gmail.com>
2023-05-16 16:40:17 +0500

    Initial commit

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 import { dogs } from './data.js';
+import { Dog } from './Dog.js';
 
-class Dog {
-  constructor(data) {
-    Object.assign(this, data);
-  }
 
-  getHtml() {
-    const { avatar, name, age, bio } = this;
-    return `
-    <div class='doggy-data'>
-      <img class='dog-avatar' src="${avatar}">
-      <div class='dog-card'>
-        <h4 class='dog-name'>${name}, ${age} <br> ${bio}</h4>
-      </div>
-    </div>
-  `;
-  }
-}
 
 let currentDogIndex = 0;
 let currentDog = new Dog(dogs[currentDogIndex]);
